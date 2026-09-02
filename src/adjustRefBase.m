2b41dca38b0f105836888221b9b46568730f1a5e
jlaundre <jlaundre@mbl.edu>
2026-09-02 12:59:58 -0400

    Update adjustRefBase.m
    Change the name of reference base variable

--- src/adjustRefBase.m
+++ src/adjustRefBase.m
@@ -1,17 +1,17 @@
-function out = adjustRefBase(tblRedox,startDate,endDate, vars)
+function out = adjustRefBase(tblRedox,startDate,endDate, vars, refVars)
 % Adjust all the redox values by means of the spare reference probes
 %   When the base reference probe deviates from the extra probes 
 % This function will subtract the mean of the extra probes from the redox probes
     % Just use Ref_06 and Ref_08 since Ref_15 has an offset of about 1 mv
     % End date is when base reference probe was changed and is functioning properly 
-    mask = startsWith(tblRedox.Properties.VariableNames, "Ref_0");
+    mask = contains(tblRedox.Properties.VariableNames,refVars);
     referenceMean = mean(tblRedox(:,mask),2,"omitmissing");
     referenceMean = renamevars(referenceMean,"mean","refMean");
     mask =(referenceMean.Properties.RowTimes <= startDate) | (referenceMean.Properties.RowTimes >= endDate) ;
     referenceMean.refMean(mask) = 0; % set to 0 before startDate and after endDate
-    tblRedox = [tblRedox,referenceMean];
+    %tblRedox = [tblRedox,referenceMean];
     % Subtract the mean of extra reference probes from redox values
-    out = varfun(@(x) x - tblRedox.refMean,tblRedox,"InputVariables",vars);
+    out = varfun(@(x) x - referenceMean.refMean,tblRedox,"InputVariables",vars);
     newNames = out.Properties.VariableNames + "Adjusted";
     newNames = replace(newNames,"Fun_",""); 
     out.Properties.VariableNames = cellstr(newNames);
