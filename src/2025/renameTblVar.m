f6915150e2b02e3f30047feb4d04764f1f6ea892
jlaundre <jlaundre@mbl.edu>
2026-09-23 12:27:58 -0400

    Variables are now similar to the 2026 variables.

--- src/2025/renameTblVar.m
+++ src/2025/renameTblVar.m
@@ -11,6 +11,10 @@ switch site
   case "low"
    varNames = replace(varNames,"Redox0","L");
    varNames = replace(varNames,"Redox","L");
+   varNames = replace(varNames,"Soil_C_1","Soil1_C");
+   varNames = replace(varNames,"Soil_C_2","Soil2_C");
+   varNames = replace(varNames,"_Avg","");
+   varNames = replace(varNames,"reference","Ref_");
    varNames = replace(varNames,regexpPattern("_1$"),"_05cm");
    varNames = replace(varNames,regexpPattern("_2$"),"_10cm");
    varNames = replace(varNames,regexpPattern("_3$"),"_15cm");
@@ -18,11 +22,19 @@ switch site
     case "high"
    varNames = replace(varNames,"Redox0","H"); 
    varNames = replace(varNames,"Redox","H");
+   varNames = replace(varNames,"Soil_C_1","Soil1_C");
+   varNames = replace(varNames,"Soil_C_2","Soil2_C");
+   varNames = replace(varNames,"_Avg","");
+   varNames = replace(varNames,"reference","Ref_");
+   varNames = replace(varNames,regexpPattern("_1$"),"_05cm");
+   varNames = replace(varNames,regexpPattern("_2$"),"_10cm");
+   varNames = replace(varNames,regexpPattern("_3$"),"_15cm");
+   varNames = replace(varNames,regexpPattern("_4$"),"_30cm");
 end
 
-% Rename the temperature and reference probes
-oldNames = {'Soil_C_1_Avg','Soil_C_2_Avg','_1_Avg','_2_Avg','_3_Avg','_4_Avg'};
-newNames = {'soil1_c','soil2_c','_05cm','_10cm','_15cm','_30cm'};
-varNames = replace(varNames,oldNames,newNames);
+% % Rename the temperature and reference probes
+% oldNames = {'_1_Avg','_2_Avg','_3_Avg','_4_Avg','reference'};
+% newNames = {'_05cm','_10cm','_15cm','_30cm','Ref_'};
+% varNames = replace(varNames,oldNames,newNames);
 
 end
